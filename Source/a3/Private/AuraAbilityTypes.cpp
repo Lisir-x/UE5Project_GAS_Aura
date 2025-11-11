95c4eba0cfc94dfa193fd41e8dda3a00acbeb162
ming_xizi <2052937687@qq.com>
2025-11-11 17:57:20 +0800

    Update to Aura Ability System Globals

---
+++ Source/a3/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,116 @@
+﻿
+#include "AuraAbilityTypes.h"
+
+//自定义网络序列化
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	/*
+	 * RepBits用来存储要复制的状态信息
+	 * 一个比特位用来存储某个特定信息
+	 */
+	uint32 RepBits = 0;
+	if (Ar.IsSaving())
+	{
+		if (bReplicateInstigator && Instigator.IsValid())
+		{
+			RepBits |= 1 << 0;
+		}
+		if (bReplicateEffectCauser && EffectCauser.IsValid() )
+		{
+			RepBits |= 1 << 1;
+		}
+		if (AbilityCDO.IsValid())
+		{
+			RepBits |= 1 << 2;
+		}
+		if (bReplicateSourceObject && SourceObject.IsValid())
+		{
+			RepBits |= 1 << 3;
+		}
+		if (Actors.Num() > 0)
+		{
+			RepBits |= 1 << 4;
+		}
+		if (HitResult.IsValid())
+		{
+			RepBits |= 1 << 5;
+		}
+		if (bHasWorldOrigin)
+		{
+			RepBits |= 1 << 6;
+		}
+		if (bIsBlockedHit)
+		{
+			RepBits |= 1 << 7;
+		}
+		if (bIsCriticalHit)
+		{
+			RepBits |= 1 << 8;
+		}
+	}
+
+	/*
+	 * 将 RepBits 序列化到 Ar 中
+	 * << 表示若Ar在保存时则从右向左序列化，在加载时从左向右反序列化
+	 * 某个比特位被设置为1则进行他的相关操作
+	 */
+	Ar.SerializeBits(&RepBits, 9);
+
+	if (RepBits & (1 << 0))
+	{
+		Ar << Instigator;
+	}
+	if (RepBits & (1 << 1))
+	{
+		Ar << EffectCauser;
+	}
+	if (RepBits & (1 << 2))
+	{
+		Ar << AbilityCDO;
+	}
+	if (RepBits & (1 << 3))
+	{
+		Ar << SourceObject;
+	}
+	if (RepBits & (1 << 4))
+	{
+		SafeNetSerializeTArray_Default<31>(Ar, Actors);
+	}
+	if (RepBits & (1 << 5))
+	{
+		if (Ar.IsLoading())
+		{
+			if (!HitResult.IsValid())
+			{
+				HitResult = TSharedPtr<FHitResult>(new FHitResult());
+			}
+		}
+		HitResult->NetSerialize(Ar, Map, bOutSuccess);
+	}
+	if (RepBits & (1 << 6))
+	{
+		Ar << WorldOrigin;
+		bHasWorldOrigin = true;
+	}
+	else
+	{
+		bHasWorldOrigin = false;
+	}
+	if (RepBits & (1 << 7))
+	{
+		Ar << bIsBlockedHit;
+	}
+	if (RepBits & (1 << 8))
+	{
+		Ar << bIsCriticalHit;
+	}
+
+	if (Ar.IsLoading())
+	{
+		//只是为了初始化InstigatorAbilitySystemComponent
+		AddInstigator(Instigator.Get(), EffectCauser.Get());
+	}	
+
+	bOutSuccess = true;
+	return true;
+}
---
+++ Source/a3/Public/AuraAbilityTypes.h
@@ -0,0 +1,64 @@
+﻿
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+//自定义游戏效果上下文结构体
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+	//是否暴击、格挡
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit () const { return bIsBlockedHit; }
+
+	//设置暴击、格挡
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	
+	/** 返回用于序列化的实际结构体，子类必须重写此方法！ */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return StaticStruct();
+	}
+	
+	/** 创建此上下文的副本，用于后续修改 */
+	virtual FGameplayEffectContext* Duplicate() const
+	{
+		//创建一个新的游戏效果上下文实例，并将当前实例的内容复制到这个新实例中
+		FGameplayEffectContext* NewContext = new FGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			//对命中结果进行深拷贝
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
+	/** 自定义序列化，子类必须重写此方法 */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+	
+protected:
+	//格挡标志
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	//暴击标志
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+	
+};
+
+//针对Aura游戏效果上下文的特化模板
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true, //支持网络序列化
+		WithCopy = true		//支持复制操作
+	};
+};
